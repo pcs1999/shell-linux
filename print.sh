def4fd9ccb5558cf214084e962abdd8f1cf48307
pcs1999.chandra shekar pendem <chandupcs1999@gmail.com>
2023-03-29 19:44:50 +0530

    colours

--- print.sh
+++ print.sh
@@ -4,3 +4,7 @@ echo  -e  "\e[33m chnadra \e[0m"
 echo  -e  "\e[34m chnadra \e[0m"
 echo  -e  "\e[35m chnadra \e[0m"
 echo  -e  "\e[36m chnadra \e[0m"
+
+dengineer=chandra
+
+echo the dengineer for devops
