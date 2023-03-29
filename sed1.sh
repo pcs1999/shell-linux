211b178a197f9ee3b111d2c3722ae2114c3d4778
pcs1999.chandra shekar pendem <chandupcs1999@gmail.com>
2023-03-29 20:28:44 +0530

    colours

---
+++ sed1.sh
@@ -0,0 +1,3 @@
+linux is used as an embedded OS for a variety of applications,
+including household appliances, automotive entertainment systems and network file system appliances.
+Network OS for routers, switches, domain name system servers, home networking devices and more.
