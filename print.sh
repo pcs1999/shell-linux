1ad843f424e0c16bd2adadc0adc3283385d841f6
pcs1999.chandra shekar pendem <chandupcs1999@gmail.com>
2023-03-29 17:45:15 +0530

    colours

---
+++ print.sh
@@ -0,0 +1,6 @@
+echo  -e  "\e[31m chnadra \e[0m"
+echo  -e  "\e[32m chnadra \e[0m"
+echo  -e  "\e[33m chnadra \e[0m"
+echo  -e  "\e[34m chnadra \e[0m"
+echo  -e  "\e[35m chnadra \e[0m"
+echo  -e  "\e[36m chnadra \e[0m"
